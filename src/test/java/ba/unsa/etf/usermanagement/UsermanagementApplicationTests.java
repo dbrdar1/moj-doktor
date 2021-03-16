8c7462af0c8990d6ee4bdac642d454227f18921f
Samra <spusina1@etf.unsa.ba>
2021-03-16 22:10:37 +0100

    Inicijalni projekt

---
+++ src/test/java/ba/unsa/etf/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
