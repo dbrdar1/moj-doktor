dacdbe5ea6f856507c389e64d8e1174787264774
Samra <spusina1@etf.unsa.ba>
2021-03-17 13:13:26 +0100

    Initial

---
+++ src/test/java/ba/unsa/etf/doktordetalji/DoktordetaljiApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.doktordetalji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoktordetaljiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
