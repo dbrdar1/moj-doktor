b33b7378affcc8172eb48f341d339dc2ef40b312
Samra <spusina1@etf.unsa.ba>
2021-04-06 19:05:50 +0200

    Komunikacija

--- src/main/java/ba/unsa/etf/doktordetalji/DoktordetaljiApplication.java
+++ src/main/java/ba/unsa/etf/doktordetalji/DoktordetaljiApplication.java
@@ -2,9 +2,24 @@ package ba.unsa.etf.doktordetalji;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
 @SpringBootApplication
+@Configuration
 public class DoktordetaljiApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DoktordetaljiApplication.class, args);
--- src/main/java/ba/unsa/etf/doktordetalji/controllers/KorisnikController.java
+++ src/main/java/ba/unsa/etf/doktordetalji/controllers/KorisnikController.java
@@ -3,10 +3,18 @@ package ba.unsa.etf.doktordetalji.controllers;
 import ba.unsa.etf.doktordetalji.responses.Response;
 import ba.unsa.etf.doktordetalji.services.KorisnikService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @AllArgsConstructor
 @RestController
@@ -14,10 +22,21 @@ public class KorisnikController {
 
     private final KorisnikService korisnikService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/inicijalizacija-baze")
     public ResponseEntity<Response> inicijalizacija(){
         String poruka = korisnikService.inicijalizirajBazu();
         return ResponseEntity.ok(new Response(poruka));
+    }
+
+    @PostMapping("/pokupi-podatke-o-korisnicima")
+    public ResponseEntity<String> postKorisnici(){
+
+        String fooResourceUrl = "http://user-management/getKorisnici";
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+        return response;
     }
 
 }
