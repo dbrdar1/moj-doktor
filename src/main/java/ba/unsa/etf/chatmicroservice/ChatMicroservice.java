78658f92c8c5570ea981225e2bb4c5b9487d5b5f
avid-dz <44071738+avid-dz@users.noreply.github.com>
2021-05-03 03:58:16 +0200

    Add interceptor

--- src/main/java/ba/unsa/etf/chatmicroservice/ChatMicroservice.java
+++ src/main/java/ba/unsa/etf/chatmicroservice/ChatMicroservice.java
@@ -1,10 +1,29 @@
 package ba.unsa.etf.chatmicroservice;
 
+import ba.unsa.etf.chatmicroservice.interceptors.HTTPHandlerInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChatMicroservice {
+
+    @Bean
+    public HTTPHandlerInterceptor httpHandlerInterceptor() {
+        return new HTTPHandlerInterceptor();
+    }
+
+    @Bean
+    public WebMvcConfigurer adapter() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(httpHandlerInterceptor());
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ChatMicroservice.class, args);
--- src/main/java/ba/unsa/etf/chatmicroservice/controllers/KorisnikController.java
+++ src/main/java/ba/unsa/etf/chatmicroservice/controllers/KorisnikController.java
@@ -1,23 +1,15 @@
 package ba.unsa.etf.chatmicroservice.controllers;
 
-import ba.unsa.etf.grpc.ActionRequest;
-import ba.unsa.etf.grpc.ActionResponse;
-import ba.unsa.etf.grpc.SystemEventsServiceGrpc;
 import ba.unsa.etf.chatmicroservice.models.Korisnik;
 import ba.unsa.etf.chatmicroservice.repositories.KorisnikRepository;
 import ba.unsa.etf.chatmicroservice.responses.Response;
 import ba.unsa.etf.chatmicroservice.services.KorisnikService;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.sql.Timestamp;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,20 +33,6 @@ public class KorisnikController {
 
     @GetMapping("/korisnici/{id}")
     Korisnik one(@PathVariable Long id) throws Exception {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8866)
-                .usePlaintext()
-                .build();
-        SystemEventsServiceGrpc.SystemEventsServiceBlockingStub stub
-                = SystemEventsServiceGrpc.newBlockingStub(channel);
-        ActionResponse actionResponse = stub.registrujAkciju(ActionRequest.newBuilder()
-                .setTimestampAkcije(Timestamp.from(ZonedDateTime.now(ZoneId.of("Europe/Sarajevo")).toInstant()).toString())
-                .setNazivMikroservisa("chat")
-                .setResurs("korisnik/" + id)
-                .setTipAkcije(ActionRequest.TipAkcije.GET)
-                .setTipOdgovoraNaAkciju(ActionRequest.TipOdgovoraNaAkciju.USPJEH)
-                .build());
-        System.out.println("Response received from server:\n" + actionResponse);
-        channel.shutdown();
         return korisnikRepository.findById(id)
                 .orElseThrow(() -> new Exception("Korisnik nije pronađen"));
     }
